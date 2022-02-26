ce7674deeccd26a14eae8292847274007b5b59af
JhinAtonic <98979555+JhinAtonic@users.noreply.github.com>
2022-02-26 22:30:37 +0100

    Add files via upload

---
+++ Destination and Filter/AdminPanel/script.js
@@ -0,0 +1,19 @@
+'use strick';
+
+const backupPanel = document.querySelector('.backupPanel');
+const severPanel = document.querySelector('.serverPanel');
+const panelHeader = document.querySelector('.panelHeader');
+backupPanel.classList.add('hidden');
+
+document.addEventListener('keydown', function (event) {
+  if ((event.key = 'p')) switchPanel();
+});
+
+const switchPanel = function () {
+  backupPanel.classList.toggle('hidden');
+  severPanel.classList.toggle('hidden');
+
+  panelHeader.textContent == 'Server1'
+    ? (panelHeader.textContent = 'Server1 / LANbackup')
+    : (panelHeader.textContent = 'Server1');
+};
